2fd576bb77ca238dcecefffa2b08ab0e5908afa9
AMJ <isaelwebdeveloper@gmail.com>
2020-04-17 14:24:19 -0600

    moved static data to file and refactored blockInfo

--- src/App.js
+++ src/App.js
@@ -5,14 +5,35 @@ import BlockInfo from './components/BlockInfo/BlockInfo';
 import {connect} from 'react-redux'
 import 'aos/dist/aos.css'
 import Aos from "aos"
-
+import {staticData} from './Static'
 
 const mapStateToProps = state => ({
   divY: state.landingPage.divY
 })
 
 function App(props) {
-  
+  let animations = ['fade-in', 'fade-up', 'fade-right', 'fade-down']
+
+  let background = ['lime-green', 'very-soft-orange', 'soft-orange','moderate-pink']
+
+  console.log(staticData)
+
+  let blockInfo = staticData.map((e,i) => (
+    <div data-aos={animations[i]} className={`block-container ${background[i]}`}>
+      <BlockInfo
+        img={e.img} 
+        me={e.me} 
+        imgSrc={e.imgSrc}
+        title={e.title}
+        desc={e.desc}
+        paragraph={e.paragraph}
+        buttonTitle={e && e.buttonTitle}
+        link={e.link}
+      />
+    </div>
+  ))
+
+  console.log(blockInfo)
 
   useEffect(() => {
     // window.addEventListener('scroll', handleScroll)
@@ -40,52 +61,7 @@ function App(props) {
         </div>
         
       </div>
-      <div data-aos='fade-in' className='block-container '>
-        <BlockInfo img={true} me={true} imgSrc={'https://firebasestorage.googleapis.com/v0/b/isael-blogs.appspot.com/o/images%2Fme.jpg?alt=media'}
-          title='Isaels Lizama'
-          desc='Full Stack Developer'
-          paragraph={`Being a web developer taught me how to think critically about what im going to do and how i'm going to do it.
-          The idea of creating something from scratch that didn't exist before is amazing.
-          One of the best things about being a web developer is, new technologies come out and make the developing more interesting.
-          You'll never be bored.
-          Also, the feeling that you can work as a team, create an app that will help multiple people is one of the best feelings I have ever felt. 
-          When i'm not developing I also love doing anything that is competitive, building and fixing computers,
-          and hanging out with friends.`}
-          buttonTitle={'Visit Blog'}
-          link={'https://isaelblogs.netlify.app/'}
-        />
-      </div>
-      <div data-aos='fade-up' className='block-container lime-green' >
-        <BlockInfo img={true} imgSrc={'https://arizonamangojuice.github.io/portfolio/img/lolImprover.png'}
-          title='Lol Improver'
-          desc='Improve your gaming skills'
-          paragraph='Lol Improver is an app that lets you take notes on champions. Allowing you to see which champions you struggle against. Lol Improver lets you find player statistics. This app user React, MongoDB, Express, and mocha, chai, enzyme and, jest. You can find the repo here.'
-          link={'https://focused-nightingale-b297e9.netlify.com/'}
-          />
-      </div>
-      <div data-aos='fade-right'  className='block-container very-soft-orange'>
-        <BlockInfo img={true} imgSrc={'https://arizonamangojuice.github.io/portfolio/img/bonus-kid-banner.png'}
-          title='Bonus Kid'
-          desc='Treat Your Children'
-          paragraph={`Bonus Kid  is an app that lets parents give incentives to their kids. The parent can add tasks and rewards to each child they have. The child can send a response saying they have completed the task or buy a reward from the store the parent has made. This app uses React, MongoDB, Express, and mocha, chai, enzyme and, jest. You can find the repo here.`}
-          link={'https://focused-nightingale-b297e9.netlify.com/'}
-        />
-      </div>
-      <div data-aos='fade-down' className='block-container soft-orange'>
-        <BlockInfo img={true} imgSrc={' https://firebasestorage.googleapis.com/v0/b/isael-blogs.appspot.com/o/images%2Fisaelsblog.jpg?alt=media'}
-          title='Isaels Blog'
-          desc='Writing Helps Think'
-          paragraph='Isaels blog is a blog web page that let me posts my thoughts and things that I have been working on. People are able to see my latest blogs. I can also login to create a blog and be able to upload images.'
-          link={'https://isaelblogs.netlify.app/'}
-        />
-      </div>
-      <div data-aos='fade-right'  className='block-container moderate-pink'>
-       <BlockInfo 
-          title=''
-          desc=''
-          paragraph=''
-        />
-      </div> 
+      {blockInfo}
     </div>
   );
 }
--- src/Static/index.js
+++ src/Static/index.js
@@ -1,3 +1,46 @@
-export const static = [
-    {}
+// this will all be in a server for final 
+export const staticData = [
+    {
+        title: 'Isaels Lizama',
+        desc: 'Full Stack Developer',
+        paragraph:`Being a web developer taught me how to think critically about what im going to do and how i'm going to do it.
+        The idea of creating something from scratch that didn't exist before is amazing.
+        One of the best things about being a web developer is, new technologies come out and make the developing more interesting.
+        You'll never be bored.
+        Also, the feeling that you can work as a team, create an app that will help multiple people is one of the best feelings I have ever felt. 
+        When i'm not developing I also love doing anything that is competitive, building and fixing computers,
+        and hanging out with friends.`,
+        img:true,
+        me:true,
+        imgSrc:'https://firebasestorage.googleapis.com/v0/b/isael-blogs.appspot.com/o/images%2Fme.jpg?alt=media',
+        buttonTitle: 'Visit Blog',
+        link: 'https://isaelblogs.netlify.app/'
+    },
+    {
+        title:'Lol Improver',
+        desc:'Improve your gaming skills',
+        paragraph:'Lol Improver is an app that lets you take notes on champions. Allowing you to see which champions you struggle against. Lol Improver lets you find player statistics. This app user React, MongoDB, Express, and mocha, chai, enzyme and, jest. You can find the repo here.',
+        img: true,
+        me: false,
+        imgSrc:'https://arizonamangojuice.github.io/portfolio/img/lolImprover.png',
+        link: 'https://focused-nightingale-b297e9.netlify.com/'
+    },
+    {
+        title:'Bonus Kid',
+        desc:'Treat Your Children',
+        paragraph:`Bonus Kid  is an app that lets parents give incentives to their kids. The parent can add tasks and rewards to each child they have. The child can send a response saying they have completed the task or buy a reward from the store the parent has made. This app uses React, MongoDB, Express, and mocha, chai, enzyme and, jest. You can find the repo here.`,
+        img:true,
+        me:false,
+        imgSrc:'https://arizonamangojuice.github.io/portfolio/img/bonus-kid-banner.png',
+        link: 'https://bonus-kid-client.herokuapp.com/'
+    },
+    {
+        title:'Isaels Blog',
+        desc:'Writing Helps Think',
+        paragraph:'Isaels blog is a blog web page that let me posts my thoughts and things that I have been working on. People are able to see my latest blogs. I can also login to create a blog and be able to upload images.',
+        img:true,
+        me:false,
+        imgSrc:' https://firebasestorage.googleapis.com/v0/b/isael-blogs.appspot.com/o/images%2Fisaelsblog.jpg?alt=media',
+        link: 'https://isaelblogs.netlify.app/'
+    }
 ]
