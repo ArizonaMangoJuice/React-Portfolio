f5292361760740a75f3555c507bf18dea3914850
AMJ <isaelwebdeveloper@gmail.com>
2020-04-16 20:34:01 -0600

    added text to bio

--- src/App.js
+++ src/App.js
@@ -42,9 +42,15 @@ function App(props) {
       </div>
       <div data-aos='fade-in' className='block-container  first-margin'>
         <BlockInfo img={true} me={true} src={'https://firebasestorage.googleapis.com/v0/b/isael-blogs.appspot.com/o/images%2Fme.jpg?alt=media'}
-          title='Isaels Blog'
+          title='Isaels Lizama'
           desc='Full Stack Developer'
-          paragraph=''
+          paragraph={`Being a web developer taught me how to think critically about what im going to do and how i'm going to do it.
+          The idea of creating something from scratch that didn't exist before is amazing.
+          One of the best things about being a web developer is, new technologies come out and make the developing more interesting.
+          You'll never be bored.
+          Also, the feeling that you can work as a team, create an app that will help multiple people is one of the best feelings I have ever felt. 
+          When i'm not developing I also love doing anything that is competitive, building and fixing computers,
+          and hanging out with friends.`}
         />
       </div>
       <div data-aos='fade-up' className='block-container lime-green' >
