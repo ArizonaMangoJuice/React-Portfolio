7e11f0abc2a48568b8308fe2071fc2855a54a13e
AMJ <isaelwebdeveloper@gmail.com>
2020-04-17 14:44:28 -0600

    added footer and css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,11 @@ export default function Footer(props){
     return (
         <footer className='moderate-pink'>
             <nav className='social-media'>
-                <a data-aos='fade-down' href=''><i className="fab fa-github"></i></a>
+                <a data-aos='fade-down' href='https://github.com/ArizonaMangoJuice?tab=repositories'><i className="fab fa-github"></i></a>
                 <a data-aos='fade-down' href=''><i className="fab fa-linkedin-in"></i></a>
                 <a data-aos='fade-down' href=''><i className="fas fa-blog"></i></a>
             </nav>
-            <p>© 2020. Isael Lizama. All rights reserved. Website Design by <a>AMJ</a></p>
+            <p>© 2020. Isael Lizama. All rights reserved. Website Design by <a className='footer-link' href='https://github.com/ArizonaMangoJuice?tab=repositories'>AMJ</a></p>
         </footer>
     )
 }
