c36ff9fe24db8e6e791b56ba5c949ea9b18a2672
AMJ <isaelwebdeveloper@gmail.com>
2020-04-17 21:40:04 -0600

    added styling to text

--- src/components/BlogPreview/BlogPreview.js
+++ src/components/BlogPreview/BlogPreview.js
@@ -31,36 +31,38 @@ export default function BlogPreview(props){
 
 
     return (
-        <div data-aos='fade-down' className='blog-container moderate-pink'>
-                <h1 data-aos='fade-right' className='blog-title'>Read My Latest Posts!</h1>
+        <div className='moderate-pink'>
+            <div data-aos='fade-down' className='blog-container moderate-pink'>
+                    <h1 data-aos='fade-right' className='blog-title'>Read My Latest Posts!</h1>
+                    <div  
+                        className={`blog relative ${newLoad ? 'blog-fade' : ''}`}
+                        style={count === 0 ? {backgroundImage: `url(${data[data.length-1].imgSrc})`, backgroundPosition:'center'} : {backgroundImage: `url(${data[count - 1].imgSrc})`, backgroundPosition:'center'}}
+                    >
+                        <div className='text dark-blue-background'>
+                            <h1>{count === 0 ? data[data.length-1].title : data[count - 1].title}</h1>
+                            <h3>{count === 0 ? data[data.length-1].desc : data[count  -1].desc}</h3>
+                        </div>
+                    </div>
+                    <div 
+                        className={`blog relative blog-main ${newLoad ? 'blog-fade' : ''}`} 
+                        style={{backgroundImage: `url(${data[count].imgSrc})`, backgroundPosition:'center'}}
+                    >
+                        <div className='text dark-blue-background'>
+                            <h1>{data[count].title}</h1>
+                            <h3>{data[count].desc}</h3>
+                            <div className='loading-bar'></div>
+                        </div>
+                    </div>
                 <div  
-                    className={`blog relative ${newLoad ? 'blog-fade' : ''}`}
-                    style={count === 0 ? {backgroundImage: `url(${data[data.length-1].imgSrc})`, backgroundPosition:'center'} : {backgroundImage: `url(${data[count - 1].imgSrc})`, backgroundPosition:'center'}}
-                >
-                    <div className='text dark-blue-background'>
-                        <h1>{count === 0 ? data[data.length-1].title : data[count - 1].title}</h1>
-                        <h3>{count === 0 ? data[data.length-1].desc : data[count  -1].desc}</h3>
+                        className={`blog relative ${newLoad ? 'blog-fade' : ''}`}
+                        style={!data[count + 1] ? {backgroundImage: `url(${data[data.length-1].imgSrc})`, backgroundPosition:'center'} : {backgroundImage: `url(${data[count + 1].imgSrc})`, backgroundPosition:'center'}}
+                    >
+                        <div className='text dark-blue-background'>
+                            <h1>{!data[count + 1] ? data[data.length-1].title : data[count + 1].title}</h1>
+                            <h3>{!data[count + 1] ? data[data.length-1].desc : data[count  + 1].desc}</h3>
+                        </div>        
                     </div>
-                </div>
-                <div 
-                    className={`blog relative blog-main ${newLoad ? 'blog-fade' : ''}`} 
-                    style={{backgroundImage: `url(${data[count].imgSrc})`, backgroundPosition:'center'}}
-                >
-                    <div className='text dark-blue-background'>
-                        <h1>{data[count].title}</h1>
-                        <h3>{data[count].desc}</h3>
-                        <div className='loading-bar'></div>
-                    </div>
-                </div>
-               <div  
-                    className={`blog relative ${newLoad ? 'blog-fade' : ''}`}
-                    style={!data[count + 1] ? {backgroundImage: `url(${data[data.length-1].imgSrc})`, backgroundPosition:'center'} : {backgroundImage: `url(${data[count + 1].imgSrc})`, backgroundPosition:'center'}}
-                >
-                    <div className='text dark-blue-background'>
-                        <h1>{!data[count + 1] ? data[data.length-1].title : data[count + 1].title}</h1>
-                        <h3>{!data[count + 1] ? data[data.length-1].desc : data[count  + 1].desc}</h3>
-                    </div>        
-                </div>
+            </div>
         </div>
     )
 }
