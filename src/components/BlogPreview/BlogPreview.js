2bf7a86001a93ecf3e03729aec97dbdf9922d8b6
AMJ <isaelwebdeveloper@gmail.com>
2020-04-17 22:30:22 -0600

    tried to add animations in to see if they stik to the build

--- src/components/BlogPreview/BlogPreview.js
+++ src/components/BlogPreview/BlogPreview.js
@@ -10,16 +10,16 @@ export default function BlogPreview(props){
     let [newLoad, setNewLoad] = useState(true)
 
     useEffect(() => {
-        Aos.init({duration: 600})
+        // Aos.init({duration: 600})
 
         const interval = setInterval(() => {
           setCount(count => count + 1);
-          setNewLoad(true)
-        }, 5000);   
+          setNewLoad(newLoad => true)
+        }, 5000)
 
         const makeNew = setInterval( () => {
-            setNewLoad(false)
-        }, 4999)
+            setNewLoad(newLoad => false)
+        }, 4990)
 
         if(count === 3) setCount(0)
 
@@ -35,12 +35,12 @@ export default function BlogPreview(props){
             <div data-aos='fade-down' className='blog-container moderate-pink'>
                     <h1 data-aos='fade-right' className='blog-title'>Read My Latest Posts!</h1>
                     <div  
-                        className={`blog relative ${newLoad ? 'blog-fade' : ''}`}
+                        className={`blog relative `}
                         style={count === 0 ? {backgroundImage: `url(${data[data.length-1].imgSrc})`, backgroundPosition:'center'} : {backgroundImage: `url(${data[count - 1].imgSrc})`, backgroundPosition:'center'}}
                     >
-                        <div className='text dark-blue-background'>
-                            <h1>{count === 0 ? data[data.length-1].title : data[count - 1].title}</h1>
-                            <h3>{count === 0 ? data[data.length-1].desc : data[count  -1].desc}</h3>
+                        <div  className='text dark-blue-background'>
+                            <h1 className={`${newLoad ? 'blog-fade' : ''}`}>{count === 0 ? data[data.length-1].title : data[count - 1].title}</h1>
+                            <h3 className={`${newLoad ? 'blog-fade' : ''}`}>{count === 0 ? data[data.length-1].desc : data[count  -1].desc}</h3>
                         </div>
                     </div>
                     <div 
@@ -48,8 +48,8 @@ export default function BlogPreview(props){
                         style={{backgroundImage: `url(${data[count].imgSrc})`, backgroundPosition:'center'}}
                     >
                         <div className='text dark-blue-background'>
-                            <h1>{data[count].title}</h1>
-                            <h3>{data[count].desc}</h3>
+                            <h1 className={`${newLoad ? 'blog-fade' : ''}`}>{data[count].title}</h1>
+                            <h3 className={`${newLoad ? 'blog-fade' : ''}`}>{data[count].desc}</h3>
                             <div className='loading-bar'></div>
                         </div>
                     </div>
@@ -58,8 +58,8 @@ export default function BlogPreview(props){
                         style={!data[count + 1] ? {backgroundImage: `url(${data[data.length-1].imgSrc})`, backgroundPosition:'center'} : {backgroundImage: `url(${data[count + 1].imgSrc})`, backgroundPosition:'center'}}
                     >
                         <div className='text dark-blue-background'>
-                            <h1>{!data[count + 1] ? data[data.length-1].title : data[count + 1].title}</h1>
-                            <h3>{!data[count + 1] ? data[data.length-1].desc : data[count  + 1].desc}</h3>
+                            <h1 className={`${newLoad ? 'blog-fade' : ''}`}>{!data[count + 1] ? data[data.length-1].title : data[count + 1].title}</h1>
+                            <h3 className={`${newLoad ? 'blog-fade' : ''}`}>{!data[count + 1] ? data[data.length-1].desc : data[count  + 1].desc}</h3>
                         </div>        
                     </div>
             </div>
