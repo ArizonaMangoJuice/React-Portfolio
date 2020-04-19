44560df7141f1234c86e4ebd4e9675018d4ac710
AMJ <isaelwebdeveloper@gmail.com>
2020-04-18 20:20:45 -0600

    used axios instead of fetch

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -3,34 +3,33 @@ let REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 let url 
 
 export default function BlogCard(props){
-    console.log('props in blog card', props)
-    if(!props.result) return <div>{props.count}}</div>
+    if(!props.result || props.result.length === 0) return <div>LOADING</div>
     
     // console.log('result', props.result)
 
-    // if(props.left){
+    if(props.left){
 
-    //     url = props.count === 0 ? props.result[props.result.length - 1].linkName : props.result[props.count - 1].linkName 
+        url = props.count === 0 ? props.result[props.result.length - 1].linkName : props.result[props.count - 1].linkName 
 
-    //     url = encodeURI(`https://isaelblogs.netlify.app/blogPages/${url}`)
+        url = encodeURI(`https://isaelblogs.netlify.app/blogPages/${url}`)
         
-    //     return (
-    //         <a 
-    //             href={url} 
-    //             className='blog relative' 
-    //             style={props.count === 0 ? {backgroundImage: `url(${props.result[props.result.length - 1].pictures[0][0].link})`, backgroundPosition:'center'} : {backgroundImage: `url(${props.result[props.count - 1].pictures[0][0].link})`, backgroundPosition:'center'}}
-    //         > 
-    //             <div className='text dark-blue-background'>
-    //                 <h1>{props.count === 0 ? props.result[props.result.length-1].title : props.result[props.count - 1].title}</h1>
-    //                 {/* <h3>{props.count === 0 ? props.result[props.result.length-1].desc : props.result[props.count  -1].desc}</h3> */}
-    //             </div>
-    //         </a>
-    //     )
-    // }
+        return (
+            <a 
+                href={url} 
+                className='blog relative' 
+                style={props.count === 0 ? {backgroundImage: `url(${props.result[props.result.length - 1].pictures[0][0].link})`, backgroundPosition:'center'} : {backgroundImage: `url(${props.result[props.count - 1].pictures[0][0].link})`, backgroundPosition:'center'}}
+            > 
+                <div className='text dark-blue-background'>
+                    <h1>{props.count === 0 ? props.result[props.result.length-1].title : props.result[props.count - 1].title}</h1>
+                    {/* <h3>{props.count === 0 ? props.result[props.result.length-1].desc : props.result[props.count  -1].desc}</h3> */}
+                </div>
+            </a>
+        )
+    }
 
     if(props.main){
-        console.log('inside main', props.result.data[props.count].linkName)
-        url = props.result.data[props.count].linkName
+
+        url = props.result[props.count].linkName
 
         url = encodeURI(`https://isaelblogs.netlify.app/blogPages/${url}`)
 
@@ -38,10 +37,10 @@ export default function BlogCard(props){
             <a  
                 href={url} 
                 className={`blog relative`}
-                style={{backgroundImage: `url(${props.result.data[props.count].pictures[0][0].link})`, backgroundPosition:'center'}}
+                style={{backgroundImage: `url(${props.result[props.count].pictures[0][0].link})`, backgroundPosition:'center'}}
             >
                 <div className='text dark-blue-background'>
-                    <h1>{props.result.data[props.count].title}</h1>
+                    <h1>{props.result[props.count].title}</h1>
                     {/* <h3>{props.result[props.count].desc}</h3> */}
                     <div className='loading-bar'></div>
                 </div>        
@@ -49,22 +48,22 @@ export default function BlogCard(props){
         )
     }
 
-    // if(props.result ){
-    //     url = !props.result[props.count + 1] ? props.result[props.result.length - 1].linkName : props.result[props.count + 1].linkName
+    if(props.result ){
+        url = !props.result[props.count + 1] ? props.result[props.result.length - 1].linkName : props.result[props.count + 1].linkName
 
-    //     url = encodeURI(`https://isaelblogs.netlify.app/blogPages/${url}`)
-    // }
+        url = encodeURI(`https://isaelblogs.netlify.app/blogPages/${url}`)
+    }
 
-    // return (
-    //     <a  
-    //         href={url}
-    //         className={`blog relative`}
-    //         style={!props.result[props.count + 1] ? {backgroundImage: `url(${props.result[props.result.length - 1].pictures[0][0].link})`, backgroundPosition:'center'} : {backgroundImage: `url(${props.result[props.count + 1].pictures[0][0].link})`, backgroundPosition:'center'}}
-    //     >
-    //         <div className='text dark-blue-background'>
-    //             <h1>{!props.result[props.count + 1] ? props.result[props.result.length-1].title : props.result[props.count + 1].title}</h1>
-    //             {/* <h3>{!props.result[props.count + 1] ? props.result[props.result.length-1].desc : props.result[props.count  + 1].desc}</h3> */}
-    //         </div>        
-    //     </a>
-    // )
+    return (
+        <a  
+            href={url}
+            className={`blog relative`}
+            style={!props.result[props.count + 1] ? {backgroundImage: `url(${props.result[props.result.length - 1].pictures[0][0].link})`, backgroundPosition:'center'} : {backgroundImage: `url(${props.result[props.count + 1].pictures[0][0].link})`, backgroundPosition:'center'}}
+        >
+            <div className='text dark-blue-background'>
+                <h1>{!props.result[props.count + 1] ? props.result[props.result.length-1].title : props.result[props.count + 1].title}</h1>
+                {/* <h3>{!props.result[props.count + 1] ? props.result[props.result.length-1].desc : props.result[props.count  + 1].desc}</h3> */}
+            </div>        
+        </a>
+    )
 }
--- src/components/BlogPreview/BlogPreview.js
+++ src/components/BlogPreview/BlogPreview.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Component } from 'react'
 import './BlogPreview.css'
 // import {staticData} from '../../Static'
 import 'aos/dist/aos.css'
@@ -6,64 +6,100 @@ import Aos from "aos"
 import {useInterval} from '../UseInterval/UseInterval'
 import BlogCard from '../BlogCard/BlogCard'
 
-import axios from 'axios'
-
 let REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 console.log(REACT_APP_SERVER_URL)
-
-export default function BlogPreview(props){
-    // let data = [...staticData]
-    let [count, setCount] = useState(0)
-    let [newLoad, setNewLoad] = useState('')
-    let [result, setResult] = useState(false)
-
-    if(count === 3) setCount(0)
-
-    useInterval(() => {
-            setCount(count + 1)
-    }, 5000)
+export default function Clock(){
+    const [currentCount, setCount] = useState(0)
+    const [fetchResult, setFetchResult] = useState('')
 
     useEffect(() => {
-        Aos.init({duration: 600})
+        if(currentCount === 4) setCount(0)
 
-        if(!result) {
-            axios.get(`${REACT_APP_SERVER_URL}/api/page/blogpreview`)
-                .then(response => setResult(response))
-                .catch(err => console.log)
-            // fetch(`${REACT_APP_SERVER_URL}/api/page/blogpreview`)
-            // .then(res => res.json())
-            // .then(result => setResult(prevResult => result))
+        if(fetchResult === ''){
+            fetch(`${REACT_APP_SERVER_URL}/api/page/blogpreview`)
+                .then(res => res.json())
+                .then(result => setFetchResult(result))
         }
         
-        return () => {
-        };
-      }, [result]);
+        const id = setInterval(() => setCount(currentCount => currentCount + 1), 5000)
 
-    // console.log('result', result)
+        return () => {
+            clearInterval(id)
+        }
+    }, [currentCount, fetchResult])
+
     return (
-        <div className='moderate-pink'>
-            <div data-aos='fade-down' className='blog-container moderate-pink'>
-                    <h1 data-aos='fade-right' className='blog-title'>Read My Latest Posts!</h1>
-                {/* <BlogCard 
-                    result={result}
-                    // data={data}
-                    count={count}
-                    left={true}
-                /> */}
-                <BlogCard 
-                    result={result}
-                    // data={data}
-                    count={count}
-                    main={true}
-                />
-                {/* <BlogCard 
-                    result={result}
-                    // data={data}
-                    count={count}
-                /> */}
-            </div>
+        <div>
+            {currentCount}{fetchResult === '' ? null : fetchResult[currentCount].linkName}
         </div>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function BlogPreview(props){
+//     // let data = [...staticData]
+//     let [count, setCount] = useState(0)
+//     let [newLoad, setNewLoad] = useState('')
+//     let [result, setResult] = useState(false)
+
+//     if(count === 3) setCount(0)
+
+//     useInterval(() => {
+//             setCount(count + 1)
+//     }, 5000)
+
+//     useEffect(() => {
+//         Aos.init({duration: 600})
+
+//         if(!result) {
+//             fetch(`${REACT_APP_SERVER_URL}/api/page/blogpreview`)
+//             .then(res => res.json())
+//             .then(result => setResult(prevResult => result))
+//         }
+        
+//         return () => {
+//         };
+//       }, [result]);
+
+//     // console.log('result', result)
+//     return (
+//         <div className='moderate-pink'>
+//             <div data-aos='fade-down' className='blog-container moderate-pink'>
+//                     <h1 data-aos='fade-right' className='blog-title'>Read My Latest Posts!</h1>
+//                 <BlogCard 
+//                     result={result}
+//                     // data={data}
+//                     count={count}
+//                     left={true}
+//                 />
+//                 <BlogCard 
+//                     result={result}
+//                     // data={data}
+//                     count={count}
+//                     main={true}
+//                 />
+//                 <BlogCard 
+//                     result={result}
+//                     // data={data}
+//                     count={count}
+//                 />
+//             </div>
+//         </div>
+//     )
+// }
 
