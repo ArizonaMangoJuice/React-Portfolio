c0dda8a18e94bafd296906d6faff7ad1aee8c2b3
AMJ <isaelwebdeveloper@gmail.com>
2020-06-23 10:34:50 -0600

    fixed responsive design

--- src/components/BlockInfo/BlockInfo.js
+++ src/components/BlockInfo/BlockInfo.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import {connect} from 'react-redux'
 import 'aos/dist/aos.css'
 import Aos from "aos"
@@ -9,20 +9,20 @@ const mapStateToProps = state => ({
 
 const BlockInfo = React.forwardRef((props, ref) => {
   // if(props.forwardRef) console.log(props, ref)
-  
   let image;
 
   useEffect(() => {
     Aos.init({duration: 1500})
   })
 
+
   if(props.img && props.me) image = (
-    <div data-aos='zoom-out-left' className='me'>
+    <div data-aos='fade-down' className='me'>
       <img className='' src={props.imgSrc} />
     </div>)
 
   if(props.img && !props.me) image = (
-    <div data-aos='zoom-out-left' className='block-image-container'>
+    <div data-aos='fade-down' className='block-image-container'>
       <img src={props.imgSrc} />
     </div>
   )
@@ -32,12 +32,12 @@ const BlockInfo = React.forwardRef((props, ref) => {
         <div className='block-info ' ref={props.forwardRef}>
             <div className='theme-bar'></div>
             <section>
-              <h4 data-aos='fade-left' className='small-padding-top-bottom'>{props.title}</h4>
+              <h4 data-aos='flip-up' className='small-padding-top-bottom'>{props.title}</h4>
               <h2 data-aos='fade-right'className='small-padding-top-bottom'>{props.desc}</h2>
               <p data-aos='fade-down' className='small-padding-top-bottom'>{props.paragraph}</p>
               <a href={props.link ? props.link : '#'} data-aos='zoom-out-down' className='block-info-button'>{props.buttonTitle ? props.buttonTitle : 'View Project'}</a>
             </section>
-            {image ? image : <h1 data-aos='zoom-out-left' className='big-quote'>
+            {image ? image : <h1 data-aos='fade-down' className='big-quote'>
                Just Do It
             </h1>}
         </div>
